17d3299efb54c564f46cd3f556e182e3dcdbf88b
shinthom <thomas@tokamak.network>
2026-09-15 15:11:21 +0900

    chore(build): add Spotless and apply google-java-format

--- src/main/java/com/example/credos_settlement/CredosSettlementApplication.java
+++ src/main/java/com/example/credos_settlement/CredosSettlementApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CredosSettlementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CredosSettlementApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CredosSettlementApplication.class, args);
+  }
 }
